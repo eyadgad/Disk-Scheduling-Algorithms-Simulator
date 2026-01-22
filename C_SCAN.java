5e3f181aeed165d8a2939afc073e941cb1fd845c
Eyad Gad <egad@uwo.ca>
2026-01-22 01:44:01 +0000

    Update C_SCAN

--- C_SCAN.java
+++ C_SCAN.java
@@ -7,12 +7,21 @@ public class C_SCAN extends DiskSchedulingAlgorithm {
     public C_SCAN(List<Integer> requests, int initialPosition) {
         super(requests, initialPosition);
     }
+    
+    public C_SCAN(List<Integer> requests, int initialPosition, DiskConfig config) {
+        super(requests, initialPosition, config);
+    }
 
     @Override
     public int execute() {
         List<Integer> localRequests = new ArrayList<>(requests);
         Collections.sort(localRequests);
         int position = initialPosition;
+        boolean movingRight = isInitialDirectionRight();
+        
+        int upperBound = getUpperCylinder();
+        int lowerBound = getLowerCylinder();
+        int diskSize = upperBound - lowerBound;
 
         while (!localRequests.isEmpty()) {
             if (localRequests.contains(position)) {
@@ -20,12 +29,24 @@ public class C_SCAN extends DiskSchedulingAlgorithm {
                 localRequests.remove(Integer.valueOf(position));
             }
 
-            position++;
-            movement++;
-
-            if (position > UPPER_CYLINDER) {
-                position = LOWER_CYLINDER;
-                movement += UPPER_CYLINDER;
+            if (movingRight) {
+                position++;
+                movement++;
+                
+                if (position > upperBound) {
+                    // Wrap around to start
+                    position = lowerBound;
+                    movement += diskSize; // Cost of wrap-around
+                }
+            } else {
+                position--;
+                movement++;
+                
+                if (position < lowerBound) {
+                    // Wrap around to end
+                    position = upperBound;
+                    movement += diskSize; // Cost of wrap-around
+                }
             }
         }
 
