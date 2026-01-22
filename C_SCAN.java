c1fa410a622fa96018f462fddf337e07c26479e3
Eyad Gad <egad@uwo.ca>
2026-01-22 02:16:39 +0000

    Update C_SCAN

--- C_SCAN.java
+++ C_SCAN.java
@@ -21,31 +21,77 @@ public class C_SCAN extends DiskSchedulingAlgorithm {
         
         int upperBound = getUpperCylinder();
         int lowerBound = getLowerCylinder();
-        int diskSize = upperBound - lowerBound;
-
-        while (!localRequests.isEmpty()) {
-            if (localRequests.contains(position)) {
-                logService(position, "C-SCAN");
-                localRequests.remove(Integer.valueOf(position));
-            }
-
-            if (movingRight) {
-                position++;
-                movement++;
-                
-                if (position > upperBound) {
-                    // Wrap around to start
-                    position = lowerBound;
-                    movement += diskSize; // Cost of wrap-around
-                }
+        
+        // Separate requests into left and right of initial position
+        List<Integer> leftRequests = new ArrayList<>();
+        List<Integer> rightRequests = new ArrayList<>();
+        
+        for (int req : localRequests) {
+            if (req < position) {
+                leftRequests.add(req);
+            } else if (req > position) {
+                rightRequests.add(req);
             } else {
-                position--;
-                movement++;
+                // Request at current position - service immediately
+                logService(position, "C-SCAN");
+            }
+        }
+        
+        // Sort both lists ascending
+        Collections.sort(rightRequests);
+        Collections.sort(leftRequests);
+        
+        if (movingRight) {
+            // Service right side first
+            for (int req : rightRequests) {
+                movement += req - position;
+                position = req;
+                logService(position, "C-SCAN");
+            }
+            
+            // If there are requests on the left, wrap around
+            if (!leftRequests.isEmpty()) {
+                // Go to upper bound
+                movement += upperBound - position;
+                position = upperBound;
                 
-                if (position < lowerBound) {
-                    // Wrap around to end
-                    position = upperBound;
-                    movement += diskSize; // Cost of wrap-around
+                // Wrap to lower bound (this is the wrap distance)
+                movement += upperBound - lowerBound;
+                position = lowerBound;
+                
+                // Service left side (now continuing in same direction from lower bound)
+                for (int req : leftRequests) {
+                    movement += req - position;
+                    position = req;
+                    logService(position, "C-SCAN");
+                }
+            }
+        } else {
+            // Moving left: service left side first (in descending order)
+            Collections.sort(leftRequests, Collections.reverseOrder());
+            
+            for (int req : leftRequests) {
+                movement += position - req;
+                position = req;
+                logService(position, "C-SCAN");
+            }
+            
+            // If there are requests on the right, wrap around
+            if (!rightRequests.isEmpty()) {
+                // Go to lower bound
+                movement += position - lowerBound;
+                position = lowerBound;
+                
+                // Wrap to upper bound (this is the wrap distance)
+                movement += upperBound - lowerBound;
+                position = upperBound;
+                
+                // Service right side (in descending order, continuing in same direction)
+                Collections.sort(rightRequests, Collections.reverseOrder());
+                for (int req : rightRequests) {
+                    movement += position - req;
+                    position = req;
+                    logService(position, "C-SCAN");
                 }
             }
         }
